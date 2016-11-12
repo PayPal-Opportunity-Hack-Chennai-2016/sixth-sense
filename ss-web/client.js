f12d7cb7b7367ef67e28a55b475546b825382931
Midhun <mparattile@paypal.com>
2016-11-12 19:22:13 +0530

    UI changes

--- ss-web/client.js
+++ ss-web/client.js
@@ -6,9 +6,6 @@ var app        = express();
 var port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
-//app.use(express.static("./app"));
-
-var routes = require('./routes');
 
 app.set('views', __dirname + '/public/views');
 app.set('view engine', 'ejs');
@@ -24,6 +21,13 @@ app.get("/login", function(req, res) {
     //res.sendFile("./app/index.html");
     //res.sendFile(__dirname+"/public/template/index");
     res.render('login', {});
+});
+
+
+app.get("/blank", function(req, res) {
+    //res.sendFile("./app/index.html");
+    //res.sendFile(__dirname+"/public/template/index");
+    res.render('blank', {});
 });
 
 // Start Server
